fd1f33be24bb5cc52a46e9f84a956d8d23ee71d3
rasmusfl0e <rasmusfl0e@gmail.com>
2015-10-12 01:51:35 +0200

    working code

---
+++ index.js
@@ -0,0 +1,46 @@
+var React = require("react");
+var server = require("react-dom/server");
+
+var matcherHelper = require("posthtml-css-matcher-helper");
+
+var attrToProp = {
+	"class": "className",
+	"for": "htmlFor"
+};
+
+function toReact (node, components) {
+	var tag = node.tag;
+	var attrs = node.attrs;
+	var element = (tag in components) ? components[tag] : (tag in React.DOM) ? tag : "div";
+	var props = {
+		key: Math.random().toString(32).slice(2)
+	};
+	
+	if (attrs) {
+		Object.keys(attrs).map(function (attr) {
+			var prop = attr in attrToProp ? attrToProp[attr] : attr;
+			props[prop] = attrs[attr];
+		});
+	}
+	
+	var children = null;
+	if (Array.isArray(node.content)) {
+		children = node.content.map(function (_node) {
+			return typeof _node === "string" ? _node : toReact(_node, components);
+		});
+	}
+	
+	return React.createElement(element, props, children);
+}
+
+module.exports = function (matcher, components) {
+	
+	return function (tree) {
+		
+		tree.match(matcherHelper(matcher), function (node) {
+			return server.renderToStaticMarkup(toReact(node, components));
+		});
+		
+	};
+	
+};
